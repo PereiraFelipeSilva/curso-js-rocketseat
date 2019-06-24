5fe7441b95ab78557d8bc905450c62f2691bda85
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-06-24 13:59:32 -0300

    ex.002 do modulo1 finalizado

---
+++ modulo1/exercicios/ex.002/index.js
@@ -0,0 +1,9 @@
+function pares(x, y) {
+   for(var x = x; x <= y; x +=2){
+      console.log(x);
+   };
+};
+
+pares(32, 321);
+
+/* Crie uma função que dado um intervalo (entre x e y) exiba todos número pares: */
