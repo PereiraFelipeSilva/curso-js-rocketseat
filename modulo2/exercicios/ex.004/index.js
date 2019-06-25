ab61c1730962210965c5d2a0bef07c40efe81157
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-06-25 11:56:35 -0300

    ex.004 do modulo2 finalizado

---
+++ modulo2/exercicios/ex.004/index.js
@@ -0,0 +1,29 @@
+/* Seguindo o resultado do exercício anterior adicione um input em tela e um botão como a seguir:
+
+<input type="text" name="nome">
+<button onClick="adicionar()">Adicionar</button>
+Ao clicar no botão, a função adicionar() deve ser disparada adicionando um novo item a lista de
+nomes baseado no nome preenchido no input e renderizando o novo item em tela juntos aos
+demais itens anteriores. Além disso, o conteúdo do input deve ser apagado após o clique. */
+
+let nomes = ["Diego", "Gabriel", "Lucas"];
+let lista = document.querySelector('.lista');
+let input = document.querySelector('#input');
+let button = document.querySelector('#button');
+
+function addNome(nome) {
+   nome = input.value;
+   nomes.push(nome);
+}
+
+button.addEventListener('click', ()=>{
+   addNome();
+   for(nome of nomes){
+      let li = document.createElement('li');
+      li.innerHTML = nome;
+      lista.appendChild(li); 
+   };
+   nomes = [];
+   input.value = '';
+   input.focus();
+});
